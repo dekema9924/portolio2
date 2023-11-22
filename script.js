28053c0960a7671d842ff2b6753bd030320af48b
Daniel Unknown <danielunknown@Daniels-MBP.attlocal.net>
2023-11-22 00:13:15 -0500

    added animations

--- script.js
+++ script.js
@@ -24,21 +24,53 @@ let service_btn = document.getElementById('service-btn');
 let project_btn = document.getElementById('project-btn');
 let contact_btn = document.getElementById('contact-btn');
 
-function getHome(){
-    home.style.transform = 'translateX(0%)';
-    service.style.transform = 'translateX(100%)';
-    project.style.transform = 'translateX(100%)';
-    contact.style.transform = 'translateX(100%)';
-
-}
-
-function getService(){
+service_btn.addEventListener('click',()=>{
     home.style.transform = 'translateX(100%)';
-    service.style.transform = 'translateX(0%)';
-    project.style.transform = 'translateX(100%)';
-   contact.style.transform = 'translateX(100%)';
+    project.style.transform = 'translateX(200%)';
+    contact.style.transform = 'translateX(200%)';
+    service.style.transform = 'translateX(0px)';
+    service.style.position = 'absolute'
+    service.style.marginLeft = '120px'
 
-}
+
+})
+home_btn.addEventListener('click',()=>{
+    home.style.transform = 'translateX(0px)';
+    service.style.transform = 'translateX(200%)';
+    project.style.transform = 'translateX(200%)';
+    contact.style.transform = 'translateX(200%)';
+    home.style.position = 'absolute'
+     home.style.marginLeft = '120px'
+
+
+
+})
+
+project_btn.addEventListener('click',()=>{
+    home.style.transform = 'translateX(200%)';
+   project.style.position = 'absolute'
+    service.style.transform = 'translateX(200%)';
+    project.style.transform = 'translateX(0px)';
+    contact.style.transform = 'translateX(200%)';
+    project.style.marginLeft = '120px'
+
+
+})
+
+contact_btn.addEventListener('click',()=>{
+    home.style.transform = 'translateX(200%)';
+    service.style.transform = 'translateX(200%)';
+    project.style.transform = 'translateX(200%)';
+    contact.style.transform = 'translateX(0px)';
+    contact.style.position = 'absolute'
+    contact.style.marginLeft = '120px'
+
+
+
+})
+
+
+
 
 
 
